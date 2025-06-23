dadb5e78ecf62d1421f640f1a5727133c2c4fe2d
unknown <Zhanel0707K@gmail.com>
2025-06-24 06:40:50 +1000

    add some code for telegramm bot

--- telegram-bot/bot/client.go
+++ telegram-bot/bot/client.go
@@ -1,1 +1,26 @@
 package bot
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"telegram-bot/config"
+	"telegram-bot/models"
+)
+
+func GetLatestPrograms() ([]models.Program, error) {
+	url := fmt.Sprintf("%s/programs", config.BackendURL)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var programs []models.Program
+	if err := json.NewDecoder(resp.Body).Decode(&programs); err != nil {
+		return nil, err
+	}
+
+	return programs, nil
+}
--- telegram-bot/main.go
+++ telegram-bot/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"log"
+
+	"telegram-bot/bot"
+	"telegram-bot/config"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	config.Load()
+
+	if config.TelegramToken == "" {
+		log.Fatal("TELEGRAM_TOKEN не задан")
+	}
+
+	botAPI, err := tgbotapi.NewBotAPI(config.TelegramToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	botAPI.Debug = true
+	log.Printf("Authorized on account %s", botAPI.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := botAPI.GetUpdatesChan(u)
+
+	for update := range updates {
+		bot.HandleMessage(botAPI, update)
+	}
+}
