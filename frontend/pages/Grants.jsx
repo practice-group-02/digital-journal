d2991de5123eb9b3438c50af34ad9d6c6a8ea4d4
unknown <Zhanel0707K@gmail.com>
2025-06-24 06:30:06 +1000

    add pages for site in backend

---
+++ frontend/pages/Grants.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Grants = () => {
+  return (
+    <div>
+      <h1>Гранты 🔍</h1>
+      <p>Здесь будет поиск и фильтрация по программам.</p>
+    </div>
+  );
+};
+
+export default Grants;
