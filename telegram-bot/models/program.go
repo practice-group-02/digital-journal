94104918f4d7a4dc1ebb6e57561ee5a34141443a
unknown <Zhanel0707K@gmail.com>
2025-06-24 05:23:33 +1000

    add structure for bot and telegramm token

--- telegram-bot/models/program.go
+++ telegram-bot/models/program.go
@@ -1,3 +1,9 @@
 package models
 
-
+type Program struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Country     string `json:"country"`
+	Deadline    string `json:"deadline"`
+	Description string `json:"description"`
+}
