7e6eeae3847f76e2286de443c9b15ed88133da3e
kobaoo <koblandikz@gmail.com>
2025-06-25 18:40:50 +0500

    Trying to create register

--- backend/go.mod
+++ backend/go.mod
@@ -1,5 +1,10 @@
 module digital-journal
 
-go 1.22
+go 1.23.0
 
-require github.com/lib/pq v1.10.9
+toolchain go1.23.10
+
+require (
+	github.com/lib/pq v1.10.9
+	golang.org/x/crypto v0.39.0
+)
---
+++ backend/internal/handlers/utils.go
@@ -0,0 +1,15 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func WriteJSONResponse(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	if data != nil {
+		json.NewEncoder(w).Encode(data)
+	}
+}
