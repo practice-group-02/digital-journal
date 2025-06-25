08470a168d89cbca44efc72224a61876d1287e50
assemqb <asemqb@gmail.com>
2025-06-26 00:13:49 +0500

    Working docker

--- backend/migrations/init.sql
+++ backend/migrations/init.sql
@@ -7,7 +7,7 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username TEXT NOT NULL UNIQUE,
     email TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
+    password_hash TEXT NOT NULL,
     role TEXT NOT NULL DEFAULT 'user',
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
