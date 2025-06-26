38a892e41faec15659b4bed0684cc28a849901f1
kobaoo <koblandikz@gmail.com>
2025-06-26 15:28:35 +0500

    Login and register endpoints

--- backend/internal/handlers/programHandlers.go
+++ backend/internal/handlers/programHandlers.go
@@ -6,7 +6,10 @@ import (
 	"encoding/json"
 	"log/slog"
 	"net/http"
+	"strings"
 	"sync"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 var usersMu sync.Mutex
@@ -24,6 +27,19 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Wrong dataType of User", http.StatusBadRequest)
 		return
 	}
+	if strings.TrimSpace(user.Username) == "" || strings.TrimSpace(user.Email) == "" || strings.TrimSpace(user.PasswordHashed) == "" {
+		slog.Error("username, email and password are required", "op", op)
+		http.Error(w, "Username, email and password are required!", http.StatusBadRequest)
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.PasswordHashed), bcrypt.DefaultCost)
+	if err != nil {
+		slog.Error("failed to hash user password", "error", err)
+		http.Error(w, "Failed to regiester", http.StatusInternalServerError)
+		return
+	}
+	user.PasswordHashed = string(hashedPassword)
 
 	usersMu.Lock()
 	defer usersMu.Unlock()
@@ -33,7 +49,7 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Failed to register!", http.StatusInternalServerError)
 		return
 	} else if exists {
-		slog.Error("failed to check existance of user", "op", op)
+		slog.Error("user already exists", "op", op)
 		http.Error(w, "User already exists!", http.StatusConflict)
 		return
 	}
@@ -55,4 +71,42 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func LoginUser(w http.ResponseWriter, r *http.Request) {
+	op := "POST /login"
+	
+	user := models.NewUser()
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		slog.Error("failed to decode user", "error", err)
+		http.Error(w, "Wrong dataType of User", http.StatusBadRequest)
+		return
+	}
+
+	if strings.TrimSpace(user.Email) == "" || strings.TrimSpace(user.PasswordHashed) == "" {
+		slog.Error("username, email and password are required", "op", op)
+		http.Error(w, "Username, email and password are required!", http.StatusBadRequest)
+		return
+	}
+
+	usersMu.Lock()
+	defer usersMu.Unlock()
+
+	if exists, err := service.UserExists(user.Email, user.Username); err != nil {
+		slog.Error("failed to check existance of user", "error", err, "op", op)
+		http.Error(w, "Failed to login!", http.StatusInternalServerError)
+		return
+	} else if !exists {
+		slog.Error("user not found", "op", op)
+		http.Error(w, "User not exists!", http.StatusNotFound)
+		return
+	}
+
+	err = service.VerifyPassword(user.Email, user.PasswordHashed)
+	if err != nil {
+		slog.Error("failed to verify password", "error", err, "op", op)
+		http.Error(w, "Wrong credentials!", http.StatusBadRequest)
+		return 
+	}
+
+	slog.Info("successfully logged in", "op", op)
+	WriteJSONResponse(w, http.StatusOK, map[string]string{"message": "Successful login"})
 }
